fb694b41efe26c04ed4c217989de1e440eb8afad
Brian Steffens <briansteffens@gmail.com>
2016-05-19 18:02:20 +0000

    Add strstr

---
+++ strstr.asm
@@ -0,0 +1,55 @@
+.include "common.inc"
+
+.section .text
+
+.globl strstr
+.type strstr, @function
+
+strstr:
+    pushl %ebp
+    movl %esp, %ebp
+
+    subl $4, %esp                  # Reserve local storage
+    movl $-1, -4(%ebp)             # Haystack loop iterator
+
+    movl $0, %edx                  # Haystack iterator
+
+strstr_haystack_loop_start:
+    movl -4(%ebp), %ecx            # Grab haystack iterator
+    incl %ecx                      # Next haystack char
+    movl %ecx, -4(%ebp)            # Save haystack iterator for next loop
+    movl $0, %edx                  # Reset needle iterator
+
+strstr_needle_loop_start:
+    movl 12(%ebp), %ebx            # Load haystack
+    movb (%ebx, %ecx, 1), %al      # Get haystack byte
+
+    cmpb $0, %al                   # End of haystack => no match
+    je strstr_end_no_match
+
+    movl 8(%ebp), %ebx             # Load needle
+    movb (%ebx, %edx, 1), %ah      # Get needle byte
+
+    cmpb $0, %ah                   # End of needle => match
+    je strstr_end_match
+
+    incl %ecx                      # Next char
+    incl %edx
+
+    cmpb %al, %ah
+    je strstr_needle_loop_start    # Matching so far, keep checking this offset
+
+    jmp strstr_haystack_loop_start # No match, try next haystack offset
+
+strstr_end_match:
+    movl -4(%ebp), %eax            # Return haystack iterator
+    jmp strstr_ret
+
+strstr_end_no_match:
+    movl $-1, %eax                 # Return -1
+    jmp strstr_ret
+
+strstr_ret:
+    movl %ebp, %esp
+    popl %ebp
+    ret
---
+++ test_strstr.asm
@@ -0,0 +1,20 @@
+.include "common.inc"
+
+.section .data
+
+    haystack: .ascii "Greetings!\12\0"
+    needle: .ascii "ing\0"
+
+.section .text
+
+.globl _start
+
+_start:
+    push $haystack          # String to search
+    push $needle            # String to search for
+    call strstr
+    addl $8, %esp
+
+    movl %eax, %ebx
+    movl $SYS_EXIT, %eax
+    int $LINUX
